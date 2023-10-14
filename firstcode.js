ea18a7c141606e1344aa59fe2c4381ffa2392f2b
SelmanKarisan <ahmetselmankarisan@gmail.com>
2023-10-14 20:47:37 +0300

    a

--- firstcode.js
+++ firstcode.js
@@ -5,3 +5,12 @@ console.log(name);
 
 const  PI = 3.14;
 console.log(PI);
+
+
+function counter(start, end) {
+    for (let i = start; i <= end; i++) {
+      console.log(i);
+    }
+  }
+  
+  counter(1, 10); // prints numbers 1 to 10
