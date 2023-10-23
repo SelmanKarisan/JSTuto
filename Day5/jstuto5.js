3b3358cd4459beb1118e50517d51aa100d7b8cad
SelmanKarisan <ahmetselmankarisan@gmail.com>
2023-10-23 22:29:42 +0300

    a

--- Day5/jstuto5.js
+++ Day5/jstuto5.js
@@ -1,5 +1,26 @@
 console.log("Welcome to Day 5 of Java Script Tutorial")
 
+//const  arr = Array()
+
+const  arr = [] //boş bir dizi oluşturmak için
+
+const  numbers = [0, 3.14, 9.81, 37, 98.6, 100]
+
+// farklı veri tipleri içerebilir
+const  arr2 = [
+
+    'Asabeneh',
+    
+    250,
+    
+    true,
+    
+    { country:  'Finland', city:  'Helsinki' },
+    
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+    
+    ]
+
 
 // LEVEL 1 EXERCISES 
   
